75280d46b9b5081a1ae867068ba943fe47e2f578
Octaviano De La Torre <octavianodelatorre@gmail.com>
2020-05-24 17:04:08 -0500

    Agregar icono de camara

--- image/src/main/java/com/example/image/ImageActivity.java
+++ image/src/main/java/com/example/image/ImageActivity.java
@@ -1,17 +1,33 @@
 package com.example.image;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class ImageActivity extends Activity {
 
-    private static boolean initOpenCV;
+    public static final String IMAGE_URI = "imageUri";
 
+    private ImageView imageView;
+
+    private static boolean initOpenCV;
     static { initOpenCV = OpenCVLoader.initDebug(); }
 
     @Override
@@ -22,5 +38,54 @@ public class ImageActivity extends Activity {
         if (initOpenCV){
             Toast.makeText(this,"OpenCV iniciado",Toast.LENGTH_LONG).show();
         }
+
+        Bundle parametros = this.getIntent().getExtras();
+        String imageUri = parametros.getString(IMAGE_URI);
+
+        imageView = findViewById(R.id.imageView);
+        imageView.setImageURI(Uri.parse(imageUri));
+    }
+
+    public void escalaGrises(View v){
+        Bitmap bmp = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+        Mat tmp = new Mat (bmp.getWidth(), bmp.getHeight(), CvType.CV_8UC1);
+        Utils.bitmapToMat(bmp, tmp);
+        Imgproc.cvtColor(tmp, tmp, Imgproc.COLOR_RGB2GRAY);
+        Utils.matToBitmap(tmp, bmp);
+        imageView.setImageBitmap(bmp);
+    }
+
+    public void guardarImagen(View v){
+        //Obtener imagen del imageview
+        imageView.buildDrawingCache();
+        Bitmap bitmap = imageView.getDrawingCache();
+
+        OutputStream fileOutStream = null;
+        Uri uri;
+        try {
+            //Crear instancia del directorio
+            File file = new File(Environment.getExternalStorageDirectory()
+                    + File.separator + "Imagenes_Editadas" + File.separator);
+            //Crear directorio
+            file.mkdirs();
+
+            //Crear archivo de imagen
+            File directorioImagenes = new File(file, "esalade_grises.jpg");
+            uri = Uri.fromFile(directorioImagenes);
+            fileOutStream = new FileOutputStream(directorioImagenes);
+        } catch (Exception e) {
+            Log.e("ERROR!", e.getMessage());
+        }
+
+        try {
+            //Guardar imagen
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutStream);
+            fileOutStream.flush();
+            fileOutStream.close();
+        } catch (Exception e) {
+            Log.e("ERROR!", e.getMessage());
+        }
+
+        Toast.makeText(this,"Imagen guardada!!",Toast.LENGTH_SHORT).show();
     }
 }
